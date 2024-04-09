234858d8f07818ec126cf6ebd2d6b1e85eb32426
Cristian Riano <cristian.riano@glovoapp.com>
2024-04-09 15:16:42 +0200

    Open and sealed classes examples

--- lib/src/main/kotlin/com/example/SealedClasses.kt
+++ lib/src/main/kotlin/com/example/SealedClasses.kt
@@ -1,3 +1,23 @@
 package com.example
 
-class ParentClass {}
+import java.io.File
+import javax.sql.DataSource
+
+// Create a sealed interface
+sealed interface Error
+
+// Create a sealed class that implements sealed interface Error
+sealed class IOError(): Error
+
+// Define subclasses that extend sealed class 'IOError'
+class FileReadError(val file: File): IOError()
+class DatabaseError(val source: DataSource): IOError()
+
+// Create a singleton object implementing the 'Error' sealed interface
+object RuntimeError : Error
+
+fun log(e: Error) = when(e) {
+  is FileReadError -> println("Error while reading file ${e.file}")
+  is DatabaseError -> println("Error while reading from database ${e.source}")
+  RuntimeError -> println("Runtime error")
+}
